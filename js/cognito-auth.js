a437b6f9eb27c297c2a4e24f8d94c911e418dee0
Ward Parks <wardparks@Wards-MacBook-Pro.local>
2026-03-25 14:36:34 +0530

    Add functional registration handler for Cognito

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -1,21 +1,44 @@
-var WildRydes = window.WildRydes || {};
-
-(function ($) {
+/*global AmazonCognitoIdentity $ _config*/
+(function() {
     var poolData = {
         UserPoolId: _config.cognito.userPoolId,
         ClientId: _config.cognito.userPoolClientId
     };
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-    if (!(_config.cognito.userPoolId && _config.cognito.userPoolClientId && _config.cognito.region)) {
-        // Show a warning if config is incomplete
-        if (typeof $ !== "undefined") {
-            $('#noCognitoMessage').show();
-        } else {
-            alert("No Cognito User Pool Configured!");
-        }
-        return;
+    function register(email, password, onSuccess, onFailure) {
+        var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute({ Name: 'email', Value: email });
+        userPool.signUp(email, password, [attributeEmail], null, function(err, result) {
+            if (!err) {
+                onSuccess(result);
+            } else {
+                onFailure(err);
+            }
+        });
     }
 
-    // Place your Cognito login, register, verification JS logic here
+    function handleRegister(event) {
+        event.preventDefault();
+        var email = $('#emailInputRegister').val();
+        var password = $('#passwordInputRegister').val();
+        var password2 = $('#password2InputRegister').val();
 
-}(jQuery));
+        if (password !== password2) {
+            alert('Passwords do not match');
+            return;
+        }
+        register(email, password,
+          function success(result) {
+              alert('Registration successful! Check your email for a code to verify your account.');
+              window.location.href = 'verify.html';
+          },
+          function failure(err) {
+              alert(err.message || JSON.stringify(err));
+          }
+        );
+    }
+
+    $(function() {
+        $('#registrationForm').submit(handleRegister);
+    });
+})();
