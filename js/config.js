cd50f982139beac8fc2cae8a89ff001bcc5d22c4
Ward Parks <wardparks@Wards-MacBook-Pro.local>
2026-03-25 14:21:44 +0530

    Update Cognito config with new App Client ID (no secret)

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_LovOoIbly',           // Your User Pool ID
-        userPoolClientId: 'rg4bedstl03jhn2s3t1tgu46v', // Your App Client ID (no secret)
-        region: 'ap-south-1'                           // Your region
+        userPoolId: 'ap-south-1_LovOoIbly',
+        userPoolClientId: '5ld70i3tmel8tk4e1qmemqktob',
+        region: 'ap-south-1'
     },
     api: { invokeUrl: '' }
 };
