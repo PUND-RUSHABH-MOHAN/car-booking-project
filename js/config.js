6cd00ce3f7b56dd80bb39380c70904f273c8401a
PUND RUSHABH MOHAN <rushabpund@gmail.com>
2026-03-25 16:51:02 +0530

    Update API invokeUrl in config.js

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@ window._config = {
         userPoolClientId: '5ld70i3tmel8tk4e1qmemqktob',
         region: 'ap-south-1'
     },
-    api: { invokeUrl: '' }
+    api: { invokeUrl: 'https://46dvxvmspi.execute-api.ap-south-1.amazonaws.com/prod' }
 };
