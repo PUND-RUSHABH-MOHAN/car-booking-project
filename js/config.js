ea708a0368fa9f1362cbef1b96271aecbb5c8e71
Ward Parks <wardparks@Wards-MacBook-Pro.local>
2026-03-25 13:56:35 +0530

    Add correct Cognito credentials to js/config.js for frontend auth

--- js/config.js
+++ js/config.js
@@ -1,10 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_8f6D0LI5g', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'crgrg1v40odp5tq2v4jbia2lt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
+        userPoolId: 'ap-south-1_LovOoIbly',           // Your User Pool ID
+        userPoolClientId: 'rg4bedstl03jhn2s3t1tgu46v', // Your App Client ID (no secret)
+        region: 'ap-south-1'                           // Your region
     },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
-    }
+    api: { invokeUrl: '' }
 };
